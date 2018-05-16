94c0aa402c7f4e07d393110331251ae249523069
Kaio Turubia <kaio_kot@hotmail.com>
2018-05-16 19:39:17 -0300

    loading image example

--- src/creator/creator.js
+++ src/creator/creator.js
@@ -1,7 +1,11 @@
+let img;
+
 function setup() {
-  // put setup code here
+  createCanvas(1280, 1967);
+  img = loadImage("samples/01.jpg");
 }
 
 function draw() {
-  // put drawing code here
+  image(img, 0, 0);
+  image(img, 0, height, img.width / 2, img.height / 2);
 }

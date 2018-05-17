e7a5d0d7209c99b37590e100e8cc69647b9860ab
Kaio Turubia <kaio_kot@hotmail.com>
2018-05-17 13:09:00 -0300

    adding responsive hackings

--- src/creator/creator.js
+++ src/creator/creator.js
@@ -1,11 +1,14 @@
 let img;
 
 function setup() {
-  createCanvas(1280, 1967);
+  // createCanvas(1280, 1967);
+  createCanvas(displayWidth, displayHeight);
   img = loadImage("samples/01.jpg");
 }
 
 function draw() {
   image(img, 0, 0);
   image(img, 0, height, img.width / 2, img.height / 2);
+  fill(255);
+  ellipse(width / 2, height / 2, 100, 100);
 }
